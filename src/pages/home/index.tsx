53a1569e36328d02ebae30396eb7f46211c0a4c1
Renan Oliveira <rotrenanoliveira@gmail.com>
2023-01-24 00:39:11 -0300

    Inserindo lista de impressoras

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,54 @@
-import { HomeContainer } from "./styles";
+import { Envelope, Printer, XCircle } from "phosphor-react";
+
+import {
+  ContentContainer,
+  HomeContainer,
+  InkStatus,
+  PrinterContainer,
+  PrinterInfo,
+  PrintersContainer,
+  RequestInkButton,
+} from "./styles";
+
+import pmeLogo from "../../assets/logo.png";
 
 export function Home() {
-  return <HomeContainer>Impressoras</HomeContainer>;
+  const hasAlert = false;
+
+  return (
+    <HomeContainer>
+      <header>
+        <img src={pmeLogo} alt="" />
+      </header>
+
+      <ContentContainer>
+        <PrintersContainer>
+          <PrinterContainer hasAlert={hasAlert}>
+            <div className="icon">
+              <Printer size={32} weight={"thin"} />
+            </div>
+
+            <PrinterInfo>
+              <strong>konica minolta bizhub c284</strong>
+              <span>setor</span>
+              <InkStatus hasAlert={hasAlert}>
+                <span>Status do estoque:</span>
+                <strong>
+                  em falta
+                  <XCircle size={32} weight="thin" />
+                </strong>
+              </InkStatus>
+            </PrinterInfo>
+
+            {hasAlert && (
+              <RequestInkButton>
+                <Envelope size={32} weight="thin" />
+                <strong>Solicitar tinta!</strong>
+              </RequestInkButton>
+            )}
+          </PrinterContainer>
+        </PrintersContainer>
+      </ContentContainer>
+    </HomeContainer>
+  );
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,3 +1,96 @@
 import styled from "styled-components";
 
-export const HomeContainer = styled.div``;
+export const HomeContainer = styled.div`
+  width: calc(100vw - 4rem);
+  margin: 2rem auto;
+
+  @media (min-width: 1366px) {
+    width: 1120px;
+  }
+`;
+
+export const ContentContainer = styled.div`
+  /* display: flex; */
+  /* flex-direction: row; */
+`;
+
+export const PrintersContainer = styled.section`
+  width: 100%;
+  margin-top: 2rem;
+
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 2rem;
+`;
+
+interface InkStatusProps {
+  hasAlert: boolean;
+}
+
+export const PrinterContainer = styled.div<InkStatusProps>`
+  display: flex;
+  align-items: flex-start;
+  gap: 1rem;
+
+  padding: 1.5rem;
+  border-radius: 8px;
+  background-color: ${(props) => (props.hasAlert ? props.theme["red-100"] : props.theme["blue-100"])};
+`;
+
+export const PrinterInfo = styled.div`
+  strong,
+  span {
+    text-transform: uppercase;
+    display: block;
+  }
+`;
+
+export const InkStatus = styled.div<InkStatusProps>`
+  margin-top: 1rem;
+
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  span {
+    font-size: 0.875rem;
+    text-transform: none;
+  }
+
+  strong {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.25rem;
+
+    color: ${(props) => (props.hasAlert ? props.theme["red-500"] : props.theme["green-300"])};
+  }
+`;
+
+export const RequestInkButton = styled.button`
+  border: 0;
+  border-radius: 8px;
+  background: transparent;
+
+  flex: 1;
+  margin-left: 2rem;
+  height: 100%;
+
+  gap: 0.5rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+
+  transition: all 0.2s;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f2dee0;
+  }
+
+  strong {
+    font-size: 0.875rem;
+  }
+`;
