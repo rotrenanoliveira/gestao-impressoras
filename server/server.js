5734bce29f3e68e35a23466a41dfc4a64f8a465e
Renan Oliveira <rotrenanoliveira@gmail.com>
2023-01-26 14:57:15 -0300

    corrigindo filepath

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 import express from "express";
 import cors from "cors";
+import { fileURLToPath } from "node:url";
 
 import { apiRoutes } from "./api/routes.js";
 
 const SERVER_PORT = 80;
 // files path
-const appPath = new URL("../dist/", import.meta.url);
-const appHMTLPath = new URL("../dist/index.html", import.meta.url);
+const appPath = fileURLToPath(new URL("../dist/", import.meta.url));
+const appHTMLPath = fileURLToPath(new URL("../dist/index.html", import.meta.url));
 
 //=
 const app = express();
 // middlewares
 app.use(cors());
-app.use(express.static(appPath.pathname));
+app.use(express.static(appPath));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,7 +22,7 @@ app.use("/api/v1", apiRoutes);
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(appHMTLPath.pathname);
+  res.sendFile(appHTMLPath);
 });
 
 app.listen(SERVER_PORT, () => {
