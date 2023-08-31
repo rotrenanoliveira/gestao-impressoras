080d27fdd0d24e0b24483fb2d2c54f49621b7eb5
Renan Oliveira <rotrenanoliveira@gmail.com>
2023-08-31 11:28:10 -0300

    style: eslint format

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -15,21 +15,13 @@ const manrope = Manrope({
   weight: ['300', '400', '500', '700', '800'],
 })
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${manrope.className} bg-[#f2f3f5] p-4 box-border w-screen h-screen lg:gap-2 lg:flex`}
-      >
+      <body className={`${manrope.className} bg-[#f2f3f5] p-4 box-border w-screen h-screen lg:gap-2 lg:flex`}>
         <Sidebar />
 
-        <main className="w-full h-full bg-white rounded-3xl overflow-auto">
-          {children}
-        </main>
+        <main className="w-full h-full bg-white rounded-3xl overflow-auto">{children}</main>
       </body>
     </html>
   )
--- client/src/components/NavBar/index.tsx
+++ client/src/components/NavBar/index.tsx
@@ -5,19 +5,9 @@ export function NavBar() {
   return (
     <nav className="mt-12">
       <ul className="flex justify-center items-center flex-col space-y-4">
-        <NavLink
-          to="/computers"
-          path="/computers"
-          navTitle="computers"
-          icon={<Laptop2 size={32} strokeWidth={1} />}
-        />
+        <NavLink to="/computers" path="/computers" navTitle="computers" icon={<Laptop2 size={32} strokeWidth={1} />} />
 
-        <NavLink
-          to="/printers"
-          path="/printers"
-          navTitle="printers"
-          icon={<Printer size={32} strokeWidth={1} />}
-        />
+        <NavLink to="/printers" path="/printers" navTitle="printers" icon={<Printer size={32} strokeWidth={1} />} />
       </ul>
     </nav>
   )
